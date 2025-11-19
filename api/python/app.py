77fa3c9d55683354c50a216271785e4f07723305
yepterence <terence.yep@gmail.com>
2025-11-19 11:29:33 -0500

    Add changes to stream chat with llm

--- api/python/app.py
+++ api/python/app.py
@@ -1,61 +1,27 @@
-# app.py
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from typing import Dict, List
+import json
 
-from sanic import Sanic, Request, Websocket
-from sanic.response import json as jsonify
-from sanic.log import logger
-import websockets
-from chatbot import get_chatbot_response, HardCodedChatbot, TorchChatbot
-from config import load_config
+from chatbot import stream_chat
 
-app = Sanic(__name__)
-app.config.from_object(load_config())
+app = FastAPI()
 
-# Use the HardCodedChatbot by default
-chatbot = HardCodedChatbot()
+async def event_stream(messages):
+    async for chunk in stream_chat(messages):
+        yield f"data: {json.dumps({'delta': chunk})}\n\n"
 
-
-class ChatRoom:
-    def __init__(self):
-        self.clients = set()
-
-    async def join(self, ws):
-        self.clients.add(ws)
-
-    async def leave(self, ws):
-        self.clients.remove(ws)
-
-    async def broadcast(self, sender, message):
-        for client in self.clients:
-            if client != sender:
-                try:
-                    await client.send(message)
-                except websockets.ConnectionClosed as e:
-                    logger.error(f"ConnectionClosed: {e}")
-
-
-chat_room = ChatRoom()
-
-
-@app.route("/")
-async def hello_world(request):
-    return jsonify({"hello": "world"})
-
-
-@app.websocket("/ws")
-async def chat(request: Request, ws: Websocket):
-    await chat_room.join(ws)
-
-    try:
-        while True:
-            message = await ws.recv()
-            chatbot_response = await get_chatbot_response(message, chatbot)
-            await chat_room.broadcast(ws, f"User #{id(ws)}: {message}")
-            await chat_room.broadcast(ws, f"{chatbot_response}")
-    except websockets.exceptions.ConnectionClosed as e:
-        logger.error(f"ConnectionClosed: {e}")
-    finally:
-        await chat_room.leave(ws)
-
-
-if __name__ == "__main__":
-    app.run(host=app.config.HOST, port=app.config.PORT, protocol="websocket")
+@app.post("/chat")
+async def chat(payload: Dict):
+    """Stream chat response. Expected message body format:
+    {
+        "messages": [
+            {"role":"user", "content": "some messages"}
+        ]
+    }
+    """
+    messages: List[Dict] = payload.get("messages", [])
+    return StreamingResponse(
+        event_stream(messages),
+        media_type="text/event-stream"
+    )
