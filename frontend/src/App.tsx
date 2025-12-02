4a7a8eab559b2ca64a123d93b583fdac3f22d930
yepterence <terence.yep@gmail.com>
2025-12-01 22:45:31 -0500

    Added initial styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,8 @@ import { ChatWindow } from "./components/ChatWindow";
 
 export default function App() {
   return (
-    <div
-      style={{
-        maxWidth: "600px",
-        margin: "2rem auto",
-        fontFamily: "sans-serif",
-      }}
-    >
-      <h1>Simple Chat</h1>
+    <div>
+      <h1 className="chat-title">Simple Chat</h1>
       <ChatWindow />
     </div>
   );
