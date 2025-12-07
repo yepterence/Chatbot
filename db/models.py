47f65b1b2f666d1bc02782c93e84aae47ead1211
yepterence <terence.yep@gmail.com>
2025-12-06 22:29:10 -0500

    Add foreign keys and link the two tables

--- db/models.py
+++ db/models.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import os
-from sqlalchemy import Column, String, Text, DateTime, Integer, create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+
+from sqlalchemy import Column, ForeignKey, String, Text, DateTime, Integer
+from sqlalchemy.orm import DeclarativeBase, relationship
 from datetime import datetime as dt
 
 
@@ -15,11 +15,13 @@ class ChatMessages(Base):
     role = Column(String, nullable=False)
     content = Column(Text, nullable=False)
     created_at =  Column(DateTime, default=dt.now)
+    chat_history_id_fk = Column(Integer, ForeignKey("chat_history.id"))
+    chat_history = relationship("ChatHistory", back_populates="chat_messages")
 
 class ChatHistory(Base):
     __tablename__ = "chat_history"
 
     id = Column(Integer, primary_key=True)
     chat_title = Column(String, nullable=False)
-
-
+    chat_messages_id_fk = Column(Integer, ForeignKey("chat_messages.id"))
+    chat_messages = relationship("ChatMessages", back_populates="chat_history")
