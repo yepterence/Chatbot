30f0bfc4e5454fc69c19ac5a52661a5255563ca3
yepterence <terence.yep@gmail.com>
2025-12-07 22:57:01 -0500

    use config settings - dedup settings

--- db/schema_setup.py
+++ db/schema_setup.py
@@ -1,16 +1,19 @@
 from sqlalchemy import create_engine
+
+from api.config import Settings
 from .models import Base
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+settings = Settings()
 
 def init_db():
-    DB_USER = os.getenv("DB_USER", "postgres")
-    DB_PASS = os.getenv("DB_PASS", "")
-    DB_NAME = os.getenv("DB_NAME", "chatbot")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_USER = settings.db_user
+    DB_PASS = settings.db_pass
+    DB_NAME = settings.db_name
+    DB_HOST = settings.db_host
+    DB_PORT = settings.db_port
 
     URL_DATABASE = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     engine = create_engine(URL_DATABASE)
--- db/session.py
+++ db/session.py
@@ -3,14 +3,17 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+from api.config import Settings
+
 load_dotenv()
+settings = Settings()
 
 def get_engine():
-    DB_USER = os.getenv("POSTGRES_USER", "postgres")
-    DB_PASS = os.getenv("POSTGRES_PASS", "")
-    DB_NAME = os.getenv("POSTGRES_DB_NAME", "chatbot")
-    DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
-    DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+    DB_USER = settings.db_user
+    DB_PASS = settings.db_pass
+    DB_NAME = settings.db_name
+    DB_HOST = settings.db_host
+    DB_PORT = settings.db_port
 
     URL_DATABASE = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     return create_engine(URL_DATABASE, echo=False)
